c2fd316e6106c871b31bb74042bfeb9c9e0e36d8
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-24 17:19:02 -0400

    Fix users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  get 'user/create'
-  get 'user/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get user_new_url
+    get users_new_url
     assert_response :success
   end
 
   test "should get create" do
-    get user_create_url
+    get users_create_url
     assert_response :success
   end
 
   test "should get show" do
-    get user_show_url
+    get users_show_url
     assert_response :success
   end
 
