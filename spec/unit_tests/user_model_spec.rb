d4af68cfff3459696e2b8888a31387c653199494
davidauza.engineer <davidauza01@gmail.com>
2020-02-25 12:49:21 -0500

    Add tests for the email of the User model

--- spec/unit_tests/user_model_spec.rb
+++ spec/unit_tests/user_model_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'User model', type: :model do
-  describe 'validations' do
-    let(:test_user) { User.new(name: 'Example', email: 'user@user.com') }
-    let(:user_comparison) { User.new(name: 'Compare', email: 'compare@user.com') }
+  let(:test_user) { User.new(name: 'Example', email: 'user@user.com') }
+  let(:another_user) { User.new(name: 'Compare', email: 'compare@user.com') }
+
+  describe 'name validations' do
 
     it 'user without name is invalid' do
       test_user.name = nil
@@ -16,13 +17,41 @@ RSpec.describe 'User model', type: :model do
     end
 
     it 'too long name is invalid' do
-      test_user.name = 'a'*51
+      test_user.name = 'a' * 51
       expect(test_user.valid?).to eq(false)
     end
 
     it 'repeated name is invalid' do
-      user_comparison.name = 'Example'
-      user_comparison.save
+      another_user.name = 'Example'
+      another_user.save
+      expect(test_user.valid?).to eq(false)
+    end
+  end
+
+  describe 'email validations' do
+    it 'rejects an user with empty email' do
+      test_user.email = nil
+      expect(test_user.valid?).to eq(false)
+    end
+
+    it 'rejects an user which email is too short' do
+      test_user.email = 'a@a.c'
+      expect(test_user.valid?).to eq(false)
+    end
+
+    it 'rejects an user which email is too long' do
+      test_user.email = 'a' * 141
+      expect(test_user.valid?).to eq(false)
+    end
+
+    it 'rejects an user which email is already used by other user' do
+      another_user.save
+      test_user.email = another_user.email
+      expect(test_user.valid?).to eq(false)
+    end
+
+    it 'rejects an user with an email not properly formatted' do
+      test_user.email = 'invalid@'
       expect(test_user.valid?).to eq(false)
     end
   end
