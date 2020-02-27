bd014618960a6f5c745d19f4345b15b9274e2b0b
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-27 13:17:24 -0400

    Add validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 4, maximum: 50}
   validates :date, presence: true
-  validates :description, presence: true
+  validate :date_is_in_the_future
+  validates :description, presence: true, length: {minimum: 4, maximum: 50}
   belongs_to :creator, class_name: 'User'
+
+  def date_is_in_the_future
+    errors.add(:date, "can't be in the past") if date < Time.now
+  end 
 end
